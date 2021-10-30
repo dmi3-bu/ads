f44c98a63ee48748ada27fcf7bcd36ac2c98250a
dmi3-bu <fixen94@gmail.com>
2021-10-31 02:49:22 +1000

    Логгирование

--- config/environment.rb
+++ config/environment.rb
@@ -7,12 +7,8 @@ require 'rake'
 require 'sinatra/reloader'
 require 'sinatra/activerecord'
 require 'sinatra/activerecord/rake'
+require 'sinatra/custom_logger'
 Bundler.require(:default, ENV['RACK_ENV'])
-
-# require 'fast_jsonapi'
-# require 'kaminari'
-# require 'dry-initializer'
-# require 'factory_bot'
 
 Dir['./app/models/*.rb'].sort.each { |file| require file }
 Dir['./app/concerns/*.rb'].sort.each { |file| require file }
@@ -21,3 +17,4 @@ Dir['./app/services/*.rb'].sort.each { |file| require file }
 Dir['./app/services/**/*.rb'].sort.each { |file| require file }
 Dir['./app/lib/**/*.rb'].sort.each { |file| require file }
 require './app/application'
+Dir['./config/initializers/*.rb'].sort.each { |file| require file }
